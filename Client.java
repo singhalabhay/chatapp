1d001cbc73a7d5ea6cf996589f523bf91d1d17ab
Abhay SInghal <120272972+singhalabhay@users.noreply.github.com>
2023-04-22 14:42:35 +0530

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.*;
 public class Client {
-    Thread r1,r2;
+    
     BufferedReader br;
     PrintWriter out;
     Socket socket;
@@ -33,7 +33,7 @@ public class Client {
         /* thread
         for writing
          */
-        Runnable r1=()->{
+        Runnable r2=()->{
             while(true){
                 try{
                     BufferedReader b1=new BufferedReader(new InputStreamReader(System.in));
